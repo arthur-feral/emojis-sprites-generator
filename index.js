06ea0f841a8eff0d09211bd2d8228fc1ca831dc9
arthur-feral <arthur.feral@hotmail.fr>
2016-11-14 11:49:11 +0100

    generator works

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 'use strict';
 
-let fs = require('fs');
+const _ = require('lodash');
+const fs = require('fs');
+const gm = require('gm');
+const when = require('when');
 
 let emojisSpriteGenerator = (sourcePath, destPath) => {
-
+  const path = `${process.cwd()}/${sourcePath}`;
+  const filesPaths = fs.readdirSync(path);
+  let result = null;
+  return when.promise((resolve, reject) => {
+    if (filesPaths.length) {
+      try {
+        _.each(filesPaths, (filePath) => {
+          let fileFullPath = `${path}/${filePath}`.split('/').filter((tempPath) => tempPath.length).join('/');
+          fileFullPath = `/${fileFullPath}`;
+          if (!result) {
+            result = gm(fileFullPath);
+          } else {
+            result.append(fileFullPath, true);
+          }
+        });
+        result.write(destPath, (error) => {
+          if (error) {
+            reject(error);
+          }
+          resolve();
+        });
+      } catch (error) {
+        reject(error);
+      }
+    } else {
+      reject(new Error('no images found'));
+    }
+  });
 };
 
 module.exports = emojisSpriteGenerator;
--- tests/emojisSpriteGenerator.test.js
+++ tests/emojisSpriteGenerator.test.js
@@ -1,7 +1,24 @@
 'use strict';
 
-let emojisSpriteGenerator = require('../index.js');
+const fs = require('fs');
+const emojisSpriteGenerator = require('../index.js');
+const resultFile = 'tests/emojis.png';
 
 describe('emojisSpriteGenerator', () => {
-  it('')
+  after(() => {
+    try {
+      fs.unlinkSync(resultFile);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  it('creates a sprite image', (done) => {
+    emojisSpriteGenerator('tests/images/', resultFile).then(() => {
+      expect(function() {
+        fs.readFileSync('tests/emojis.png');
+      }).to.not.throw(Error);
+      done();
+    }).catch(done);
+  });
 });
