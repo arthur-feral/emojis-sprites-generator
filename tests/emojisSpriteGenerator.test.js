dad0050d3d6835fb66de89533a9f8d3beb271439
arthur-feral <arthur.feral@hotmail.fr>
2016-11-14 11:50:54 +0100

    code style :lipstick:

--- tests/emojisSpriteGenerator.test.js
+++ tests/emojisSpriteGenerator.test.js
@@ -5,20 +5,15 @@ const emojisSpriteGenerator = require('../index.js');
 const resultFile = 'tests/emojis.png';
 
 describe('emojisSpriteGenerator', () => {
-  after(() => {
-    try {
-      fs.unlinkSync(resultFile);
-    } catch (error) {
-      console.log(error);
-    }
-  });
-
   it('creates a sprite image', (done) => {
     emojisSpriteGenerator('tests/images/', resultFile).then(() => {
       expect(function() {
         fs.readFileSync('tests/emojis.png');
       }).to.not.throw(Error);
+      fs.unlinkSync(resultFile);
       done();
     }).catch(done);
   });
+
+  // should test error cases
 });
