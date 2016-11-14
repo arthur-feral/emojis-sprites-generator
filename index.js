bf08ec65bb109206adbe73d429bfb6159c95a650
arthur-feral <arthur.feral@hotmail.fr>
2016-11-14 18:09:42 +0100

    make only one entrypoint

--- index.js
+++ index.js
@@ -3,7 +3,20 @@
 const spriteGenerator = require('./lib/spriteGenerator');
 const lessGenerator = require('./lib/lessGenerator');
 
-module.exports = {
-  spriteGenerator,
-  lessGenerator
+/**
+ * generate emojis sprite resources
+ * @param {object} config an object containing config
+ * {
+ *    {string} emojisImages path to emojis png folder,
+ *    {string} spriteDestination destination of resources,
+ *    {int} emojisSize size in px for emojis height sprite,
+ *    {object} emojisList json containing emojis list
+ * }
+ */
+const generator = (config) => {
+  spriteGenerator(config.emojisImages, config.spriteDestination, config.emojisSize).then(()=> {
+    lessGenerator(config.spriteDestination, config.emojisList, config.spriteDestination, config.emojisSize);
+  });
 };
+
+module.exports = generator;
