e2c6e01e8e6664930e3a8892d3f8bf4268c9e64f
arthur-feral <arthur.feral@hotmail.fr>
2016-11-17 14:28:58 +0100

    module can take a basic config

--- index.js
+++ index.js
@@ -5,25 +5,38 @@ const scrapper = require('./lib').scrapper;
 const fs = require('fs');
 const _ = require('lodash');
 
-const jsonPath = `${process.cwd()}/emojis.json`;
-
 const emojisModule = (config) => {
+  console.log('Starting scrapper...');
+  scrapper.scrap()
+    .then((datas) => {
+      try {
+        fs.accessSync(config.destination, fs.F_OK);
+      } catch (error) {
+        fs.mkdirSync(config.destination);
+      }
+      fs.writeFileSync(`${config.destination}/emojis.json`, JSON.stringify(datas), 'utf8');
+      console.log('Successfully writen emojis json file.');
 
+      return scrapper.scrapImages(config, datas)
+        .then((themes)=> {
+          console.log('Generating sprites...');
+          _.each(themes, (theme) => {
+            let themeDatas = _.merge({}, datas);
+            _.each(datas, (category => {
+              themeDatas[category.name].emojis = _.sortBy(_.filter(category.emojis, (emoji) => _.has(emoji.themes, theme)), 'index');
+            }));
+
+            return generator.generateSprite(theme, themeDatas, config.size, config.destination);
+          });
+        }).finally(() => {
+          console.log('Done.');
+        });
+    });
 };
 
-console.log('Starting scrapper...');
-scrapper.scrap()
-  .then((datas) => {
-    fs.writeFileSync(jsonPath, JSON.stringify(datas), 'utf8');
-    console.log('Successfully writen emojis json file.');
+emojisModule({
+  size: 48,
+  destination: 'test'
+});
 
-    return scrapper.scrapImages(datas)
-      .then((themes)=> {
-        console.log('Generating sprites...');
-        _.each(themes, (theme) => {
-          return generator.generateSprite(theme, datas);
-        });
-      }).finally(() => {
-        console.log('Done.');
-      });
-  });
+module.exports = emojisModule;
