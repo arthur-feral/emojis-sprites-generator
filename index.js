23f0f9d03c7be9857e20c5bef2112962c4cb8ba3
arthur-feral <arthur.feral@hotmail.fr>
2016-11-18 16:14:09 +0100

    done with crawler

--- index.js
+++ index.js
@@ -9,31 +9,22 @@ const _ = require('lodash');
 const emojisModule = (config) => {
   console.log('Starting scrapper...');
   scrapper.scrap(config.fromCache)
-    .then(crawl)
     .then(() => {
-      // try {
-      //   fs.accessSync(config.destination, fs.F_OK);
-      // } catch (error) {
-      //   fs.mkdirSync(config.destination);
-      // }
-      // fs.writeFileSync(`${config.destination}/emojis.json`, JSON.stringify(datas), 'utf8');
-      // console.log('Successfully writen emojis json file.');
-
-      // return scrapper.scrapImages(config, datas)
-      //   .then((themes)=> {
-      //     console.log('Generating sprites...');
-      //     _.each(themes, (theme) => {
-      //       let themeDatas = _.merge({}, datas);
-      //       _.each(datas, (category => {
-      //         themeDatas[category.name].emojis = _.sortBy(_.filter(category.emojis, (emoji) => _.has(emoji.themes, theme)), 'index');
-      //       }));
-      //
-      //       return generator.generateSprite(theme, themeDatas, config.size, config.destination);
-      //     });
-      //   }).finally(() => {
-      //     console.log('Done.');
-      //   });
-    });
+      return crawl(config);
+    })
+    .then((datas) => {
+      return scrapper.scrapImages(config, datas);
+    }).then((themes) => {
+    // console.log('Generating sprites...');
+    // _.each(themes, (theme) => {
+    //   let themeDatas = _.merge({}, datas);
+    //   _.each(datas, (category => {
+    //     themeDatas[category.name].emojis = _.sortBy(_.filter(category.emojis, (emoji) => _.has(emoji.themes, theme)), 'index');
+    //   }));
+    //
+    //   return generator.generateSprite(theme, themeDatas, config.size, config.destination);
+    // });
+  });
 };
 
 emojisModule({
