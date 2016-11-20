ff031088ae4ca047cc2f3dad971f7170f31ef7ff
arthur-feral <arthur.feral@hotmail.fr>
2016-11-20 18:33:31 +0100

    fix image generator

--- index.js
+++ index.js
@@ -2,35 +2,37 @@
 
 const generator = require('./lib').generator;
 const scrapper = require('./lib').scrapper;
-const crawl = require('./lib').crawler;
+const when = require('when');
 const fs = require('fs');
 const _ = require('lodash');
 
 const emojisModule = (config) => {
   console.log('Starting scrapper...');
   scrapper.scrap(config)
-    // .then(() => {
-    //   return crawl(config);
-    // })
     .then((datas) => {
-      //return scrapper.scrapImages(config, datas);
-    }).then((themes) => {
-    // console.log('Generating sprites...');
-    // _.each(themes, (theme) => {
-    //   let themeDatas = _.merge({}, datas);
-    //   _.each(datas, (category => {
-    //     themeDatas[category.name].emojis = _.sortBy(_.filter(category.emojis, (emoji) => _.has(emoji.themes, theme)), 'index');
-    //   }));
-    //
-    //   return generator.generateSprite(theme, themeDatas, config.size, config.destination);
-    // });
-  });
+      return when.all([
+        datas, scrapper.scrapImages(config, datas)
+      ]).spread(function(d, t) {
+        return [d, t];
+      });
+    })
+    .then((datas) => {
+      console.log('Generating sprites...');
+      return when.all(_.map(datas[1], (theme) => {
+        let themeDatas = _.merge({}, datas[0]);
+        _.each(datas[0], (category => {
+          themeDatas[category.name].emojis = _.sortBy(_.filter(category.emojis, (emoji) => _.has(emoji.themes, theme)), 'index');
+        }));
+
+        return generator.generateSprite(theme, themeDatas, config.size, config.destination);
+      }));
+    });
 };
 
 emojisModule({
   size: 24,
   destination: 'test',
-  fromCache: false
+  fromCache: true
 });
 
 module.exports = emojisModule;
