5a9d8306c577ca86be70ff8c0cb287d63c281952
arthur-feral <arthur.feral@hotmail.fr>
2016-11-26 19:03:32 +0100

    remove useless files

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 const _ = require('lodash');
 const superagent = require('superagent');
-const when = require('when');
 const lib = require('./lib')(superagent);
 
 module.exports = lib.emojiModule;
