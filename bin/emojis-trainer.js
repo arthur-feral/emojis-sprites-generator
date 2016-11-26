a3d46d4294c48c8975cfe6a0ee3a0720e61ff96e
arthur-feral <arthur.feral@hotmail.fr>
2016-11-26 19:11:38 +0100

    warn user if preproc is not supported

--- bin/emojis-trainer.js
+++ bin/emojis-trainer.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 'use strict';
 
+const _ = require('lodash');
 const commander = require('commander');
 const emojisModule = require('../index.js');
 const packagejson = require('../package.json');
+const PREPROCS = ['sass', 'less'];
+const logger = require('../lib/logger');
 
 commander
   .version(packagejson.version)
@@ -15,7 +18,8 @@ commander
   .option('-c, --cache', 'Force cache use (use last cached html and images) Dont use it if you want last released emojis')
   .parse(process.argv);
 
-if (!commander.preproc) {
+if (!commander.preproc || _.indexOf(PREPROCS, commander.preproc) === -1) {
+  logger.error(`Only ${PREPROCS} css preprocessors are supported`);
   commander.help();
 } else {
   emojisModule.run(commander);
