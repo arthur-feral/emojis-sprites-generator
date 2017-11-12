b32c2c34405fb84fcc2a49dfaff316cb0dceca7d
arthur-feral <arthur.feral@hotmail.fr>
2017-11-12 17:56:50 +0100

    add task to build dist files

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-'use strict';
+#!/usr/bin/env node
+import {
+  indexOf,
+} from 'lodash';
+import commander from 'commander';
 
-const _ = require('lodash');
-const superagent = require('superagent');
-const lib = require('./lib')(superagent);
+const emojisModule = require('./lib/index.js');
+const packagejson = require('./package.json');
+const PREPROCS = ['sass', 'less'];
+const logger = require('./lib/logger');
 
-module.exports = lib.emojiModule;
+commander
+  .version(packagejson.version)
+  .usage('[options] [value]')
+  .option('-d, --destination [path]', 'Path for generated files')
+  .option('-s, --size [size]', 'The sprite\'s height')
+  .option('--preproc [preprocessor type]', 'the css preprocessor type (less, sass etc...)')
+  .option('-p, --prefix [prefix]', 'The classnames prefix')
+  .option('-c, --cache', 'Force cache use (use last cached html and images) Dont use it if you want last released emojis')
+  .parse(process.argv);
+
+if (!commander.preproc || indexOf(PREPROCS, commander.preproc) === -1) {
+  logger.error(`Only ${PREPROCS} css preprocessors are supported`);
+  commander.help();
+
+  return;
+}
